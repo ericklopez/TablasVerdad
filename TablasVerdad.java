3a376e4eda433ae3a9b80bc73c749061c7cb166e
Erick Pollo López <Erick Pollo Lpez>
2017-02-15 14:58:58 -0600

    Se subieron los primeros archivos

---
+++ TablasVerdad.java
@@ -0,0 +1,8 @@
+public class TablasVerdad{
+	public static void main(String argv[]){
+		System.out.println("Bienvenido!");
+		System.out.println("Este programa realiza tablas de verdad");
+
+		
+	}
+}
